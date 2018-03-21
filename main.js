6389414434b4e33066fb99148cf9d8f1f89f4902
shaikzakir <shaikzakir.997@gmail.com>
2018-03-21 11:16:02 +0530

    json and js

---
+++ main.js
@@ -0,0 +1,17 @@
+//ajax initiation
+function jsondata(file,callback){
+  var xhr=new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET",file,true);
+  xhr.onreadystatechange=function(){
+    if(xhr.readyState === 4 && xhr.status ===200){
+      callback(xhr.responseText);
+    }
+  };
+  xhr.send();
+}
+//function calling
+jsondata("data.json",function(text){
+  let data=JSON.parse(text);
+  console.log(data);
+})
